ceaeb690bb5485f4a787c5087b7be7a80543700c
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-05-31 21:32:45 +0300

    feat: install radix-vue & unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import RadixVueResolver from "radix-vue/resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [RadixVueResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
