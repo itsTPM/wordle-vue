5c9d22ab70ee7070e205ad83379bb0124d3b2764
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-06-03 14:05:15 +0300

    fix(vite.config): import path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import RadixVueResolver from "radix-vue/resolver";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
